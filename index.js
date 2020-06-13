05f979f6e27f88d91290463a0ed5fabf4e51cf2c
Eva Navarro <emanao@me.com>
2020-06-13 02:23:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+
+ function wrapGifts(ary,event) {
+   const msgCollection;
+  for (let i = 0; i < ary.length; i++) {
+    msgCollection.push(`Thank you, ${ary[i]}, for the wonderful ${event} gift!`)
+  }
+
+  return msgCollection;
+}
+
 function countDown(i){
   let counter = 0;
   while(counter>=0 && counter<=i){
